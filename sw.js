2d69f3d683f549afe9ebc053738ee7ce78428656
Urocare2025 <urocare2025@gmail.com>
2026-06-17 15:00:38 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+// Simple & Reliable Service Worker for MedLedger
+const CACHE_NAME = 'medledger-v7';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    }).catch(() => caches.match('./index.html'))
+  );
+});
